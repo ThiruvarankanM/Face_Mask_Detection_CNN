875b898532f1e057bd42783a7daa00e3a32a7c22
ThiruvarankanM <thiru07official@gmail.com>
2025-09-06 12:42:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     
     img_array = np.array(image.resize((128, 128)), dtype=np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
